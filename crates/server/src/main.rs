190e702ef6ea74cf191561a91cee569bcf1522aa
josh <jmillikan12345@gmail>
2022-06-08 17:09:49 -0400

    improved documentation and misc refactoring

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,9 +1,11 @@
 use std::time::Duration;
+use log::info;
 
 const TICK_INTERVAL: Duration = Duration::from_millis(50);
 
 #[tokio::main]
 async fn main() {
+    info!("Server starting");
     let mut interval = tokio::time::interval(TICK_INTERVAL);
     loop {
         interval.tick().await;
